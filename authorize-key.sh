6415d32f3444c4c850497f7730ea0a1624d1c53b
Tomasz Klim <github@tomaszklim.pl>
2018-04-25 11:54:30 +0200

    script to install public ssh key in authorized_keys

---
+++ authorize-key.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+. /opt/farm/scripts/functions.net
+
+if [ "$3" = "" ]; then
+	echo "usage: $0 <hostname[:port]> <username> <ssh-key-path>"
+	exit 1
+elif [ "`resolve_host $1`" = "" ]; then
+	echo "error: parameter $1 not conforming hostname format, or given hostname is invalid"
+	exit 1
+elif ! [[ $2 =~ ^[a-z0-9._-]+$ ]]; then
+	echo "error: parameter $2 not conforming username format"
+	exit 1
+elif [ ! -f $3 ] || [ ! -f $3.pub ]; then
+	echo "error: key $3 not found (both private and public keys are required)"
+	exit 1
+fi
+
+server=$1
+user=$2
+newkey=$3
+keytext="`head -n1 $newkey.pub`"
+
+if [ -z "${server##*:*}" ]; then
+	host="${server%:*}"
+	port="${server##*:}"
+else
+	host=$server
+	port=22
+fi
+
+ssh -p $port -o StrictHostKeyChecking=no -o PubkeyAuthentication=no -o PasswordAuthentication=yes $user@$host \
+	"mkdir -p ~/.ssh; touch ~/.ssh/authorized_keys; echo \"$keytext\" >> ~/.ssh/authorized_keys"
+
+if [[ $? != 0 ]]; then
+	echo "error: host $host denied access (wrong password for $user@$host?)"
+	exit 1
+fi
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -h /usr/local/bin/sf-authorize-key ]; then
+	rm -f /usr/local/bin/sf-authorize-key
+fi
+
 if [ -h /usr/local/bin/sf-provision ]; then
 	rm -f /usr/local/bin/sf-provision
 fi
