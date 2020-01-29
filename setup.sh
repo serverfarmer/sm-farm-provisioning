b689dc0a2025b0351c5a85dfd8763f01f9a97edd
Tomasz Klim <github@tomaszklim.pl>
2020-01-29 23:15:54 +0100

    use the new method of handling /usr/local/bin aliases

--- setup.sh
+++ setup.sh
@@ -5,7 +5,4 @@
 mkdir -p   /var/log/provisioning /etc/local/.provisioning
 chmod 0700 /var/log/provisioning /etc/local/.provisioning
 
-ln -sf /opt/farm/ext/farm-provisioning/authorize-key.sh /usr/local/bin/sf-authorize-key
-ln -sf /opt/farm/ext/farm-provisioning/provision.sh /usr/local/bin/sf-provision
-
 /opt/farm/ext/farm-provisioning/add-profile.sh default
--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -h /usr/local/bin/sf-authorize-key ]; then
-	rm -f /usr/local/bin/sf-authorize-key
-fi
-
-if [ -h /usr/local/bin/sf-provision ]; then
-	rm -f /usr/local/bin/sf-provision
-fi
